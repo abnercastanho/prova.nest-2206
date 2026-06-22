c17201113a78d08f37c09109b0c38a9e3c1c65b9
Abner Castanho <cardosoabner64@gmail.com>
2026-06-22 20:01:57 -0300

    strutura inicial do microsservico TCP e scripts do package.json

--- src/main.ts
+++ src/main.ts
@@ -4,33 +4,33 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  // Criação do app (que funcionará como nosso API Gateway)
+  // criacao do app com o modulo principal
   const app = await NestFactory.create(AppModule);
 
-  // Requisito 2: Realizar a liberação do CORS
+  // liberação do cors
   app.enableCors({
     origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
 
-  // Ativa validação global para os DTOs do Swagger
+  // ativa a validacao de dados de entrada (DTOs) para todos os end-points
   app.useGlobalPipes(new ValidationPipe());
 
-  // Requisito 3: Ter a documentação de todos os end-points (Swagger)
+  // documentação do Swagger e endpoints de teste da API
   const config = new DocumentBuilder()
     .setTitle('Prova NestJS - API Gateway')
     .setDescription('Endpoints de Login, Criação e Consulta de Usuários')
     .setVersion('1.0')
-    .addBearerAuth() // Ativa o campo de Token JWT no Swagger
+    .addBearerAuth() 
     .build();
     
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  // O Gateway vai rodar na porta 3000
+
   await app.listen(3000);
-  console.log('🚀 API Gateway rodando em: http://localhost:3000');
-  console.log('📖 Documentação Swagger em: http://localhost:3000/api/docs');
+  console.log('API Gateway rodando em: http://localhost:3000');
+  console.log('Documentação Swagger em: http://localhost:3000/api/docs');
 }
 bootstrap();
---
+++ src/microsservico/main-microservico.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { AppModule } from '../app.module'; // Vamos centralizar no AppModule para simplificar a prova
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 3001, // Porta onde o microsserviço vai escutar o Gateway
+      },
+    },
+  );
+  
+  await app.listen();
+  console.log('🤖 Microsserviço de Autenticação/Usuários rodando via TCP na porta 3001');
+}
+bootstrap();
